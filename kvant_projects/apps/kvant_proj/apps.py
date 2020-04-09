70f4108f10239fa14c8ef026a40a64144b05fd9b
Max <vortexmax852@gmail.com>
2020-04-09 19:25:57 +0700

    создание приложения kvant_proj

---
+++ kvant_projects/apps/kvant_proj/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class KvantProjConfig(AppConfig):
+    name = 'kvant_projects.apps.kvant_proj'
--- kvant_projects/settings.py
+++ kvant_projects/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'kvant_projects.apps.kvant_proj',
 ]
 
 MIDDLEWARE = [
