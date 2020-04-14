542c764784cb1126627bc943611ce03f9b1932c3
Max <vortexmax852@gmail.com>
2020-04-14 19:40:51 +0700

    добавили класс ключевых слов

--- kvant_projects/apps/kvant_proj/admin.py
+++ kvant_projects/apps/kvant_proj/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from kvant_projects.apps.kvant_proj.models import Key_words
 # Register your models here.
+
+admin.site.register(Key_words, admin.ModelAdmin)
--- kvant_projects/apps/kvant_proj/models.py
+++ kvant_projects/apps/kvant_proj/models.py
@@ -3,6 +3,13 @@ from ckeditor_uploader.fields import RichTextUploadingField
 
 # Create your models here.
 
+class Key_words(models.Model):
+    key_word = models.CharField(max_length=200, verbose_name='ключевые слова')
+
+    def __str__(self):
+        return f"{self.key_word} ({self.id})"
+
+
  # class Post(models.Model):
  #
  #    author = models.ForeignKey('auth.User')
