5bed628f867ce0fb177045f332a29ff946bef3a5
Max <vortexmax852@gmail.com>
2020-04-14 19:19:09 +0700

    добавили отображение через шаблон

--- kvant_projects/apps/kvant_proj/views.py
+++ kvant_projects/apps/kvant_proj/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def index(request):
+    context = {
+
+    }
+    return render(request, 'index.html', context)
+   #return HttpResponse("Hello, world. You're at the polls index.")
--- kvant_projects/urls.py
+++ kvant_projects/urls.py
@@ -14,10 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from kvant_projects.apps.kvant_proj.views import index
 
 urlpatterns = [
-    url(r'^ckeditor/', include('ckeditor_uploader.urls')),
+    re_path(r'^ckeditor/', include('ckeditor_uploader.urls')),
     path('admin/', admin.site.urls),
+
+    path('', index, name='index')
+
+
+
+
 
 ]
