8d06d11da01e01486509e9488133f977be79d390
Max <vortexmax852@gmail.com>
2020-04-16 19:17:36 +0700

    добавлены основные модели

--- kvant_projects/apps/kvant_proj/admin.py
+++ kvant_projects/apps/kvant_proj/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from kvant_projects.apps.kvant_proj.models import Key_words
+from kvant_projects.apps.kvant_proj.models import Key_words, Kvantum, Kvantorianec, Master, kvant_projects
+
+
 # Register your models here.
 
-admin.site.register(Key_words, admin.ModelAdmin)
+admin.site.register(Key_words,  admin.ModelAdmin)
+admin.site.register(Kvantorianec,  admin.ModelAdmin)
+admin.site.register(Kvantum,  admin.ModelAdmin)
+admin.site.register(kvant_projects,  admin.ModelAdmin)
+admin.site.register(Master,  admin.ModelAdmin)
