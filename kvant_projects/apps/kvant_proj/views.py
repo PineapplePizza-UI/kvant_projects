de1ec3b3fd5ad6c1cea4e3f7736c7bbb51bede8c
Max <vortexmax852@gmail.com>
2020-04-28 19:27:51 +0700

    починили!!!!!!!!!

--- kvant_projects/apps/kvant_proj/views.py
+++ kvant_projects/apps/kvant_proj/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from kvant_projects.apps.kvant_proj.models import Key_words, Kvantum, Kvantorianec, Master, kvant_projects
+from kvant_projects.apps.kvant_proj.models import Key_words, Kvantum, Kvantorianec, Master, Kvant_projects
 
 
 # Create your views here.
 
 def index(request):
     kvantorianec_list = Kvantorianec.objects.all()
+    project_list = Kvant_projects.objects.all()
     context = {
-        "kv_list": kvantorianec_list
+        "kv_list": kvantorianec_list,
+        "proj_list": project_list,
     }
     return render(request, 'index.html', context)
 
@@ -28,15 +30,8 @@ def kvantum(request, kvantum_id):
     return render(request, 'kvantum.html', context)
 
 
-def project_list(request):
-    project_list = kvant_projects.objects.all()
-    context = {
-        "proj_list": project_list
-    }
-    return render(request, 'index.html', context)
-
 def kvant_project(request, project_id):
-    project = kvant_projects.objects.get(id=project_id)
+    project = Kvant_projects.objects.get(id=project_id)
     context = {
         "project": project
     }
